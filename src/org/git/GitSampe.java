d385908229f19780a4c7e4a2f2bce1cfbcf383ec
karthikbs1994 <bkarthik4195@gmai.com>
2022-04-22 11:32:00 +0530

    veu's code

--- src/org/git/GitSampe.java
+++ src/org/git/GitSampe.java
@@ -6,6 +6,8 @@ public class GitSampe {
 		System.out.println("text");
 		System.out.println("text2");
 		System.out.println("text3");
+		System.out.println("text6");
+		System.out.println("text7");
 	}
 
 }
